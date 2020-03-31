05a9d763b09c365ba2aedbac2ef0f213b4027b97
smuller97 <ksk123456@live.dk>
2020-03-31 22:32:50 +0200

    Added Jquery to the project and made a background-component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import './style.css';
+import './style.css'; //#Import of styling
+import Background from './pages/Background';
 
 export default class App extends Component {
-    render() {
-        return (
-            <div>
-                <h1 className="headerApp">
-                    REACT APP - Hello world
-                    </h1>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div>
+				<h1 className="headerApp">REACT APP - Hello world</h1>
+                <Background></Background>
+			</div>
+		);
+	}
 }
