b032991e06fdfb905c9f8684b98277083031b045
smuller97 <ksk123456@live.dk>
2020-04-01 14:39:46 +0200

    Pages, navbar and footer component files created

---
+++ src/pages/Nav.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+//TODO: Make navbar with bootstrap and reacat route
+
+export default class Nav extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ src/pages/Saying.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Saying extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
