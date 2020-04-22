503eaeef096fc583bb09a9f5592ce5e448854e2b
smuller97 <ksk123456@live.dk>
2020-04-22 14:05:49 +0200

    Update App.jsx with redirect link when app starts

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './style.css'; //#Import of styling
 import Background from './pages/Background';
 import Nav from './pages/Nav';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'; //Import of react router
+import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'; //Import of react router
 import Home from './pages/Home';
 import Calm from './pages/Calm';
 import Footer from './pages/Footer';
@@ -20,6 +20,7 @@ export default class App extends Component {
                             <Route path="/Calm" exact component={Calm}></Route>
                             <Route path="/Saying" exact component={Saying}></Route>
                             <Route path="/Thankful" exact component={Thankful}></Route>
+                            <Redirect from='/' to="/Home" /> 
                         </Switch>
                     </Router>
                 <Footer></Footer>
