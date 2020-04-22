2c8a78fb6c8e34e1508991fb84529f2c8df50266
smuller97 <ksk123456@live.dk>
2020-04-22 17:03:26 +0200

    Removed gh-pages again

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './style.css'; //#Import of styling
 import Background from './pages/Background';
 import Nav from './pages/Nav';
-import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'; //Import of react router
+import { HashRouter as Router, Route } from 'react-router-dom'; //Import of react router
 import Home from './pages/Home';
 import Calm from './pages/Calm';
 import Footer from './pages/Footer';
@@ -13,18 +13,16 @@ export default class App extends Component {
 	render() {
 		return (
 			<div>
-                <Nav></Nav>
-                    <Router>
-                        <Switch>
-                            <Route path="/Home" exact component={Home}></Route>
-                            <Route path="/Calm" exact component={Calm}></Route>
-                            <Route path="/Saying" exact component={Saying}></Route>
-                            <Route path="/Thankful" exact component={Thankful}></Route>
-                            <Redirect from='/' to="/Home" /> 
-                        </Switch>
-                    </Router>
-                <Footer></Footer>
-                <Background></Background>
+				<Nav />
+				<Router>
+					<Route path="/" component={Home} />
+					<Route path="/Calm" component={Calm} />
+					<Route path="/Saying" component={Saying} />
+					<Route path="/Thankful" component={Thankful} />
+				</Router>
+
+				<Footer />
+				<Background />
 			</div>
 		);
 	}
